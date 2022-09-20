62f48660004664db1f884f98e121593afa7a54c8
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-09-20 13:16:29 +0800

    Learned Pattern Matching

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -11,8 +11,15 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into a map:
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    # first_line = request |> String.split("\n") |> List.first
+    # [method, path, _] = String.split(first_line, " ")
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
